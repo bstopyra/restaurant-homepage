92ffd946c62c91c10302428b251a4e31773dc020
ogierhaq <ogierhaq@gmail.com>
2021-08-24 13:33:54 +0200

    menu with sweet sweet pizzas added

--- src/menu.js
+++ src/menu.js
@@ -1,20 +1,56 @@
+const pizzaMenu = [
+  {
+    id: 1,
+    name: "Ham Pizza",
+    description: "Ham, Tomato, Garlic",
+  },
+  {
+    id: 2,
+    name: "Shrimp Pizza",
+    description: "Shrimp, Olives, Feta Cheese",
+  },
+  {
+    id: 3,
+    name: "Bacon Pizza",
+    description: "Ham, Bacon, Garlic, Paprika",
+  },
+  {
+    id: 4,
+    name: "Vege Pizza",
+    description: "Tomato, Onion, Feta Cheese",
+  },
+];
+
 function createMenu() {
   const menu = document.createElement("div");
   menu.classList.add("main");
 
   const about = document.createElement("div");
-  about.classList.add("about");
+  about.classList.add("menu");
 
-  const paragraph = document.createElement("p");
-  paragraph.classList.add("about__text");
-  paragraph.innerHTML = "Menu!";
-  about.appendChild(paragraph);
+  for (let i = 0; i < pizzaMenu.length; i++) {
+    const menuPosition = document.createElement("div");
+    menuPosition.classList.add("menu-position");
 
-  const chefImg = document.createElement("img");
-  chefImg.src = "./images/chef.png";
+    const pizzaImg = document.createElement("img");
+    pizzaImg.classList.add("pizza");
+    pizzaImg.src = `images/pizza-${i + 1}.png`;
 
-  chefImg.classList.add("about__chef");
-  about.appendChild(chefImg);
+    const pizzaDescription = document.createElement("div");
+    pizzaDescription.classList.add("pizza-desc");
+
+    const pizzaTitle = document.createElement("h2");
+    pizzaTitle.textContent = pizzaMenu[i].name;
+
+    const pizzaIngredients = document.createElement("p");
+    pizzaIngredients.textContent = pizzaMenu[i].description;
+
+    menuPosition.appendChild(pizzaImg);
+    pizzaDescription.appendChild(pizzaTitle);
+    pizzaDescription.appendChild(pizzaIngredients);
+    menuPosition.appendChild(pizzaDescription);
+    about.appendChild(menuPosition);
+  }
 
   menu.appendChild(about);
 
